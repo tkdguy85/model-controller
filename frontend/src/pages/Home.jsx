67bd5610e32a7aae4bf66b974555e979ccfd068e
Dallas3 <dallasgordon3@gmail.com>
2025-12-19 14:52:30 -0800

    Update Home layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,16 +5,24 @@ function Home() {
     <div className="home-container">
       <h1 className="home-header">Model Controller Home Screen</h1>
       <p className="home-subheader">Select which model you are using below:</p>
+      
+
+      
+      {/* Model A List */}
       <ul className="model-list">
-        <li className="model-list-item">Model A - 1A (North America)</li>
-        <li className="model-list-item">Model A - 1Ax (North America Experimental)</li>
-        <li className="model-list-item">Model A - 1B (Central/South America)</li>
-        <li className="model-list-item">Model A - 2B (Europe/Asia)</li>
-        <li className="model-list-item">Model A - 2C (Africa)</li>
-        <li className="model-list-item">Model A - 2D (Australia)</li>
-        <li className="model-list-item">Model X - Test Prototype</li>
-        <li className="model-list-item">Model X - High Altitude Prototype</li>
-        <li className="model-list-item">Model X - Extended Range Prototype</li>
+        <li className="model-list-item"><button className='model-button'>Model A - 1A (North America)</button></li>
+        <li className="model-list-item"><button className='model-button'>Model A - 1Ax (North America Experimental)</button></li>
+        <li className="model-list-item"><button className='model-button'>Model A - 1B (Central/South America)</button></li>
+        <li className="model-list-item"><button className='model-button'>Model A - 2B (Europe/Asia)</button></li>
+        <li className="model-list-item"><button className='model-button'>Model A - 2C (Africa)</button></li>
+        <li className="model-list-item"><button className='model-button'>Model A - 2D (Australia)</button></li>
+      </ul>
+      
+      {/* Model X List */}
+      <ul className="model-list">
+        <li className="model-list-item"><button className='model-button'>Model X - Test Prototype</button></li>
+        <li className="model-list-item"><button className='model-button'>Model X - High Altitude Prototype</button></li>
+        <li className="model-list-item"><button className='model-button'>Model X - Extended Range Prototype</button></li>
       </ul>
     </div>
   )
